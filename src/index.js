e63ba83b40ebabd946062e8d11529bf119d0feff
aksu <Scyzhe@hotmail.com>
2022-03-14 13:03:53 +0200

    3c(3.18)

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const pNumber = require('./models/numbers')
 const { response } = require('express')
+const req = require('express/lib/request')
 const app = express()
 
 morgan.token('body', (req) => JSON.stringify(req.body));
@@ -13,27 +14,13 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
-  }
-]
-
-
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/info', (req,res) => {
-  res.send('<p>Phone book has info of ' + pNumber.length + ' people</p><p>'+ new Date() +'</p>')
+app.get('/info', (req, res) => {
+  res.send('<p>Phone book has info of ' + pNumber.length + ' people</p><p>' + new Date() + '</p>')
 })
 
 app.get('/api/persons', (req, res) => {
@@ -44,23 +31,42 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  pNumber.findById(request.params.id).then(number => {
-    response.json(number)
-  })
+  pNumber.findById(request.params.id)
+    .then(number => {
+      response.json(number)
+    })
+    .catch(e => {
+      console.log(e)
+      response.status(400).send({ error: 'malformatted id' })
+    })
+
+})
+
+app.put('/api/persons/:id', (request, response) => {
+  const body = request.body
+  try {
+  pNumber.updateOne(
+    { "_id": mongoose.Types.ObjectId(request.params.id) },
+    { $set: { number: body.number } })
+    .then(resp => {
+      console.log(resp)
+      response.status(204).end()
+    })
+  } catch (e){
+    console.log(e)
+      response.status(400).send({ error: 'malformatted id' })
+  }
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  //const id = Number(request.params.id)
-  //persons = persons.filter(person => person.id !== id)
-  //pNumber.collection.deleteOne(number => number.id !== request.params.id)
   try {
-    pNumber.deleteOne( { "id" : mongoose.Types.ObjectId(request.params.id) } )
-    .then(resp => {
-      console.log(resp)
-    })
- } catch (e) {
+    pNumber.deleteOne({ "_id": mongoose.Types.ObjectId(request.params.id) })
+      .then(resp => {
+        console.log(resp)
+      })
+  } catch (e) {
     console.log(e)
- }
+  }
   response.status(204).end()
 })
 
@@ -68,8 +74,8 @@ app.post('/api/persons', (req, res) => {
   const body = req.body
 
   if (!body.name || !body.number) {
-    return res.status(400).json({ 
-      error: 'content missing' 
+    return res.status(400).json({
+      error: 'content missing'
     })
   }
 
@@ -77,7 +83,7 @@ app.post('/api/persons', (req, res) => {
     name: body.name,
     number: body.number,
   })
-  
+
   number.save().then(response => {
     res.json(response)
   })
